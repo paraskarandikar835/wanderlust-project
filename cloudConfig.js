7a133ff71a577abf0ef2ccd08f0e853c2ee54ea9
paraskarandikar835 <paraskarandikar1@gmail.com>
2026-08-21 10:39:22 +0530

    debug cloudinary config

--- cloudConfig.js
+++ cloudConfig.js
@@ -7,6 +7,12 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
+console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
+console.log("CLOUD_API_KEY:", process.env.CLOUD_API_KEY);
+console.log(
+    "CLOUD_API_SECRET loaded:",
+    !!process.env.CLOUD_API_SECRET
+);
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
